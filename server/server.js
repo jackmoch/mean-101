f09a7ecc8eaeb9eb87c8624cd5cd0bb101ece95e
Jack Mocherman <mochermanjack@gmail.com>
2016-09-26 10:56:13 -0500

    added bootstrap

--- server/server.js
+++ server/server.js
@@ -9,4 +9,8 @@ app.set('port', port)
 
 app.use(express.static('client'))
 
+app.get('/api/title', (req, res) => 
+	res.json({ title: 'Mean 101 from Node' })
+)
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
