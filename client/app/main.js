ba9700fcc5161eabe36a7d8e2e77ad86f5ea5e6c
Jack Mocherman <mochermanjack@gmail.com>
2016-09-26 12:21:01 -0500

    using mongo to post and retrieve messages

--- client/app/main.js
+++ client/app/main.js
@@ -1,10 +1,40 @@
 'use strict';
 
 angular
-	.module('mean101', [])
-	.controller('main', function ($scope, $http) {
+	.module('mean101', ['ngRoute'])
+	.config($routeProvider => 
+		$routeProvider
+			.when('/', {
+				controller: 'MainCtrl',
+				templateUrl: 'partials/main.html'
+			})
+			.when('/chat', {
+				controller: 'ChatCtrl',
+				templateUrl: 'partials/chat.html'
+			})
+	)
+	.controller('MainCtrl', function ($scope, $http) {
 		$http.get('/api/title')
-			.then(({ data: { title } }) => {
+			.then(({ data: { title }}) => {
 				$scope.title = title
 			})
+	})
+	.controller('ChatCtrl', function ($scope, $http) {
+
+		$scope.sendMessage = function () {
+
+			const msg = {
+				author: $scope.author,
+				content: $scope.content
+			}
+
+			$http.post('/api/messages', msg)
+			.then(() => $scope.messages.push(msg))
+			.catch(console.error)
+		}
+
+		$http.get('/api/messages')
+		.then(({ data : { messages }}) => {
+			$scope.messages = messages
+		})
 	})
--- server/server.js
+++ server/server.js
@@ -1,16 +1,45 @@
 'use strict';
 
+const { json } = require('body-parser')
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
-const port = process.env.PORT || 3000
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
+const PORT = process.env.PORT || 3000
 
-app.set('port', port)
+app.set('port', PORT)
 
 app.use(express.static('client'))
+app.use(json())
 
 app.get('/api/title', (req, res) => 
-	res.json({ title: 'Mean 101 from Node' })
+	res.json({ title: 'MEAN Chat' })
 )
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const Message = mongoose.model('message', {
+	author: String,
+	content: String,
+})
+
+app.get('/api/messages', (req, res, err) => 
+	Message
+		.find({})
+		.then(messages => 
+		res.json({ messages })
+		)
+		.catch(err)
+)
+
+app.post('/api/messages', (req, res, err) => {
+	const msg = req.body
+	Message
+		.create(msg)
+		.then(msg => res.json(msg))
+		.catch(err)
+})
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () => {
+	app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+})
